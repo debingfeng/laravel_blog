74f6a79e763e1ab2c3b5fa48ed9f9250e6f5a082
Fengalbean <ifengdb@163.com>
2017-07-03 00:22:26 +0800

    init rep

---
+++ app/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $dates = ['published_at'];
+
+    //
+//    protected function getDateFormat(){
+//        return time();
+//    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+
+        if (! $this->exists) {
+            $this->attributes['slug'] = str_slug($value);
+        }
+    }
+}
---
+++ database/migrations/2017_07_02_125003_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->string('title',120);
+            $table->text('content');
+            $table->text('origin_content')->nullable();
+            $table->timestamps();
+            $table->timestamp('published_at')->default(DB::raw('CURRENT_TIMESTAMP'))->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
